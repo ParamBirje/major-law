d60ce55bfc33610bbd24386a7842ea1d0589f46a
ParamBirje <parambirje@gmail.com>
2024-06-03 12:23:04 +0530

    Added Messages History section to Chat component

--- apps/web/src/components/chat.tsx
+++ apps/web/src/components/chat.tsx
@@ -1,6 +1,8 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { RocketIcon } from "@radix-ui/react-icons";
 
 export default function Chat() {
   return (
@@ -16,7 +18,17 @@ export default function Chat() {
       </div>
 
       <div className="flex gap-5">
-        <div className="w-2/3">Messages</div>
+        <div className="w-2/3">
+          <h3 className="mb-5 scroll-m-20 text-2xl font-semibold tracking-tight">Messages</h3>
+          <div className="flex flex-col gap-5">
+            <Alert>
+              <RocketIcon className="h-4 w-4" />
+              <AlertTitle>Copilot</AlertTitle>
+              <AlertDescription>Hi! What can I help you with?</AlertDescription>
+            </Alert>
+          </div>
+        </div>
+
         <Card className="w-1/3">
           <CardHeader>
             <CardTitle className="text-lg">Web Sources</CardTitle>
