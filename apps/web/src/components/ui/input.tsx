51969a3342b7a03b783873051b00a6c204dd4da7
ParamBirje <parambirje@gmail.com>
2024-06-03 12:24:27 +0530

    Updated chat Input styles

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-12 w-full rounded-lg border border-slate-400 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 duration-200 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-black disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-12 w-full rounded-lg border-2 border-slate-300 bg-transparent px-3 py-1 text-sm transition-colors file:border-0 duration-200 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-black disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
