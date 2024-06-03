a94d687e215bac404590504f258eb7a95e7d919c
ParamBirje <parambirje@gmail.com>
2024-06-03 12:16:46 +0530

    Added web sources ui to Chat

--- apps/web/src/components/chat.tsx
+++ apps/web/src/components/chat.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 export default function Chat() {
   return (
@@ -12,6 +13,22 @@ export default function Chat() {
         />
 
         <Button className="h-auto w-[15%]">Search</Button>
+      </div>
+
+      <div className="flex gap-5">
+        <div className="w-2/3">Messages</div>
+        <Card className="w-1/3">
+          <CardHeader>
+            <CardTitle className="text-lg">Web Sources</CardTitle>
+            <CardDescription>Add URLs of the webpages you want to reference.</CardDescription>
+
+            <Button variant="outline">Add +</Button>
+          </CardHeader>
+
+          <CardContent>
+            <div>sources</div>
+          </CardContent>
+        </Card>
       </div>
     </section>
   );
