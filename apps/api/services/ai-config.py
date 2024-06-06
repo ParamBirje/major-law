5a787a18a06e3e8d7a16170b1b565903d56e75b1
ParamBirje <parambirje@gmail.com>
2024-06-06 10:15:37 +0530

    Integrated a native way to integrate chat system with Command R's api and boto3

--- apps/api/services/ai-config.py
+++ apps/api/services/ai-config.py
@@ -5,6 +5,8 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 
+from typing import List, Dict
+
 client = boto3.client("bedrock-runtime", region_name="us-east-1")
 model_id = "cohere.command-r-v1:0"
 llm = BedrockLLM(
@@ -18,10 +20,10 @@ llm = BedrockLLM(
 
 
 def one_time_lang(text: str) -> str:
-    prompt_template = PromptTemplate(
-        input_variables=["text"],
-        template="Answer this: {text}"
-    )
+    # prompt_template = PromptTemplate(
+    #     input_variables=["text"],
+    #     template="Answer this: {text}"
+    # )
     chain = ConversationChain(llm=llm, memory=ConversationBufferMemory())
 
     res = chain.predict(input=text)
@@ -29,19 +31,19 @@ def one_time_lang(text: str) -> str:
     return res
 
 
-print(one_time_lang("what are you doing"))
+# print(one_time_lang("what are you doing"))
 
 
-def one_time_response(prompt: str) -> str:
+def one_time_response(prompt: str, chat_history: List[Dict]) -> str:
     '''
     Provides a one time response for the given `prompt`
     '''
 
-    prompt = "Describe what is life in one line."
     native_request = {
         "message": prompt,
+        "chat_history": chat_history,
         "max_tokens": 1024,
-        "temperature": 0.5,
+        "temperature": 0.9,
     }
 
     request = json.dumps(native_request)
@@ -57,3 +59,15 @@ def one_time_response(prompt: str) -> str:
 
     response_text = model_response["text"]
     return response_text
+
+
+# print(one_time_response("Describe life in one line.", [
+#     {
+#         "role": "USER",
+#         "message": "Act as if you are a funny IT engineer. Answer any question I ask with a funny pun from I.T"
+#     },
+#     {
+#         "role": "CHATBOT",
+#         "message": "Okay, I will."
+#     },
+# ]))
