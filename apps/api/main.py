30da4bc563c1e80e19cf5222d854b9fedc49b2bf
ParamBirje <parambirje@gmail.com>
2024-06-07 07:55:15 +0530

    Updated main.py with comments and json response for health check

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from routes.chat import router as ChatRouter
 
 app = FastAPI()
 
+# Routers
 app.include_router(ChatRouter)
 
 
 @app.get("/")
 def home():
-    return {"Hello": "World"}
+    return JSONResponse({
+        "message": "Backend is up!"
+    }, status_code=200)
