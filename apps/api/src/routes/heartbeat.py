d62536b00472e9d817ca651b2d3d9121e76ab149
Param Birje <parambirje@gmail.com>
2024-06-07 12:07:23 +0530

    Fixed session id cookie behavior

--- apps/api/src/routes/heartbeat.py
+++ apps/api/src/routes/heartbeat.py
@@ -1,25 +1,27 @@
 from datetime import datetime
 import uuid
-
 from fastapi.responses import JSONResponse
 from src.middlewares.session import SESSION_ACTIVITY
-
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 
 router = APIRouter(prefix="/heartbeat")
 
 
 @router.get("/")
 async def heartbeat(request: Request):
+    print(request.cookies, "received.")
     session_id = request.cookies.get("session_id")
-    response = JSONResponse({"status": "ok"})
+    response = JSONResponse({
+        "status": "alive"
+    }, status_code=200)
 
     if not session_id:
         # Generate a new session ID
+        print("Generating new session ID.")
         session_id = str(uuid.uuid4())
-        response.set_cookie(key="session_id", value=session_id)
+        response.set_cookie(key="session_id", value=session_id, secure=True, samesite='None')
 
-    print(session_id)
+    print(session_id, "updated.")
 
     SESSION_ACTIVITY[session_id] = datetime.now()
     return response
--- apps/web/src/components/heartbeat.tsx
+++ apps/web/src/components/heartbeat.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 
 function Heartbeat() {
-  const intervalInMinutes = 5;
+  const intervalInMinutes = 0.2;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch("http://127.0.0.1:5656/heartbeat", {
+      fetch("http://127.0.0.1:8000/heartbeat", {
         credentials: "include", // To include cookies with the request
-      });
+      }).then(() => console.log("bhai mil ga respons"));
     }, intervalInMinutes * 60 * 1000);
 
     return () => clearInterval(interval);
