ffa68f75c96bd602cdf2b1087341948f652d8572
Param Birje <parambirje@gmail.com>
2024-06-08 10:30:19 +0530

    Implemented UI logic of sending messages and adding to the message history in /web

--- apps/web/src/components/chat/chat.tsx
+++ apps/web/src/components/chat/chat.tsx
@@ -3,43 +3,71 @@ import { Button } from "../ui/button";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { AvatarIcon, PaperPlaneIcon, RocketIcon } from "@radix-ui/react-icons";
 import WebSources from "./web-sources";
+import { useState } from "react";
+import { type Message } from "@/lib/types";
 
 export default function Chat() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  function handleSendMessage(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const message: Message = {
+      message_id: Math.random().toString(36).substring(7),
+      message: String(formData.get("message")),
+      role: "user",
+    };
+    setMessages([...messages, message]);
+    setNewMessage("");
+  }
+
   return (
     <section id="chat" className="flex flex-col gap-8">
-      <div className="flex gap-5">
+      <form onSubmit={handleSendMessage} className="flex gap-5">
         <Input
           className="w-full"
           title="Start your legal search here"
+          name="message"
           placeholder="What does the article 15 of GDPR say?"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
         />
 
-        <Button className="h-auto w-[15%] flex gap-2 items-center">
+        <Button
+          type="submit"
+          className="h-auto w-[15%] flex gap-2 items-center"
+        >
           <p>Send</p>
           <PaperPlaneIcon />
         </Button>
-      </div>
+      </form>
 
       <div className="flex flex-col-reverse md:flex-row gap-5">
         <div className="md:w-2/3">
           <h3 className="mb-5 scroll-m-20 text-2xl font-semibold tracking-tight">
             Messages
           </h3>
-          <div className="flex flex-col gap-5">
-            <Alert>
-              <RocketIcon className="h-4 w-4" />
-              <AlertTitle className="text-green-500">Assistant</AlertTitle>
-              <AlertDescription>
-                Yes certainly, what article do you need help with?
-              </AlertDescription>
-            </Alert>
-            <Alert>
-              <AvatarIcon className="h-4 w-4" />
-              <AlertTitle>User</AlertTitle>
-              <AlertDescription>
-                Hey I need help with a certain article in GDPR?
-              </AlertDescription>
-            </Alert>
+          <div className="flex flex-col-reverse gap-5">
+            {messages.map((message, i) => {
+              return (
+                <Alert key={i}>
+                  {message.role == "user" ? (
+                    <AvatarIcon className="h-4 w-4" />
+                  ) : (
+                    <RocketIcon className="h-4 w-4" />
+                  )}
+                  <AlertTitle
+                    className={`${
+                      message.role == "ai" && "text-green-500"
+                    } capitalize`}
+                  >
+                    {message.role}
+                  </AlertTitle>
+                  <AlertDescription>{message.message}</AlertDescription>
+                </Alert>
+              );
+            })}
           </div>
         </div>
 
--- apps/web/src/lib/types.ts
+++ apps/web/src/lib/types.ts
@@ -2,3 +2,10 @@ export type Webpage = {
   url: string;
   message_id?: string;
 };
+
+export type Message = {
+  message_id: string;
+  message: string;
+  role: "user" | "ai";
+  created?: number;
+};
